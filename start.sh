f26fb24ba9c33194f7bac6c98029de2d3f557756
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-05-25 19:02:20 -0400

    updated script

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# Function to stop server
+function stop_server {
+    echo "Caught SIGTERM signal! Stopping server..."
+    kill $server_pid
+}
+
+# Start the server in the background
 ./bin/redis_server &
+server_pid=$!
+
+echo "Server started pid=$server_pid"
+
+# Stop server on SIGTERM
+trap stop_server SIGTERM
+
+# Start the client
 ./bin/redis_client
